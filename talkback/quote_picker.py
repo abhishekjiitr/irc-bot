fb54d061c80ca7a0f27bd30274776e7e4eafdb57
Abhishek Jaisingh <abhi2254015@gmail.com>
2016-10-04 05:18:29 +0530

    update: quote_picker.py code

--- talkback/quote_picker.py
+++ talkback/quote_picker.py
@@ -1,1 +1,11 @@
 from random import choice
+
+class QuotePicker(object):
+	def __init__(self, quotes_filename):
+		"""Initialize QuotePicker class"""
+		with open(quotes_filename) as quotes_file:
+			self.quotes = quotes_file.readlines()
+
+	def pick(self):
+		"""Return a random quote"""
+		return choice(self.quotes).strip()
